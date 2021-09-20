e96e4b914e1810ba5c38a84c785d21933c0af04b
hxd <lzg1518964710@163.com>
2021-09-20 15:22:48 +0800

    完成了登录与注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,60 @@ $(function () {
     $('.reg-box').hide()
   })
 
+  var form = layui.form
+  var layer = layui.layer
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+
+    repwd: function (value) {
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+
+  })
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('http://api-breakingnews-web.itheima.net/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message);
+      }
+      layer.msg('注册成功，请登录！')
+      //模拟人的点击行为
+      $('#link_login').click()
+
+    })
+  })
+
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: 'http://api-breakingnews-web.itheima.net/api/login',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！');
+        }
+        else {
+          layer.msg('登录成功！')
+          localStorage.setItem('token', res.token)
+          location.href = '/index.html'
+        }
+      }
+    })
+  })
 })
 
 
